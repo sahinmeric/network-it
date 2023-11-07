8480de2c516a66b8c7beae84a66f8ace1c86b2a4
sahinmeric <sahin.meric86@gmail.com>
2023-11-07 14:19:09 +0100

    replaced route paths with constants

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Profile from "./components/Profile";
+import ROUTES from "./services/routes";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.PROFILE} element={<Profile />} />
     </Routes>
   );
 };
