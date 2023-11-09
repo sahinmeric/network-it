c92f24186dc520796d05a7aea70159acde2d5373
sahinmeric <sahin.meric86@gmail.com>
2023-11-10 00:36:01 +0100

    added mock groups data with members

---
+++ src/mockdata/groups.js
@@ -0,0 +1,584 @@
+const groups = [
+  {
+    "id": "0",
+    "name": "Bins - Muller",
+    "private": true,
+    "location": "Slovenia",
+    "members": [
+      {
+        "memberId": "1",
+        "memberName": "Elbert McGlynn",
+        "jobTitle": "Future Metrics Architect"
+      },
+      {
+        "memberId": "2",
+        "memberName": "Johnathan Kunde",
+        "jobTitle": "National Division Analyst"
+      },
+      {
+        "memberId": "3",
+        "memberName": "Krista Raynor",
+        "jobTitle": "Future Intranet Analyst"
+      },
+      {
+        "memberId": "4",
+        "memberName": "Kendra Reichert",
+        "jobTitle": "Human Research Facilitator"
+      },
+      {
+        "memberId": "5",
+        "memberName": "Nick Prosacco",
+        "jobTitle": "Senior Creative Planner"
+      },
+      {
+        "memberId": "6",
+        "memberName": "Caleb Labadie Sr.",
+        "jobTitle": "Internal Usability Supervisor"
+      },
+      {
+        "memberId": "7",
+        "memberName": "Dr. Darla Borer PhD",
+        "jobTitle": "Internal Creative Representative"
+      },
+      {
+        "memberId": "8",
+        "memberName": "Aubrey Funk",
+        "jobTitle": "Product Quality Supervisor"
+      },
+      {
+        "memberId": "9",
+        "memberName": "Jan Homenick",
+        "jobTitle": "Direct Implementation Orchestrator"
+      },
+      {
+        "memberId": "10",
+        "memberName": "Miss Camille Mraz",
+        "jobTitle": "Central Paradigm Planner"
+      }
+    ]
+  },
+  {
+    "id": "1",
+    "name": "Klein - Hyatt",
+    "private": false,
+    "location": "Gabon",
+    "members": [
+      {
+        "memberId": "1",
+        "memberName": "Velma Konopelski",
+        "jobTitle": "International Quality Consultant"
+      },
+      {
+        "memberId": "2",
+        "memberName": "Eula Kiehn",
+        "jobTitle": "Global Optimization Associate"
+      },
+      {
+        "memberId": "3",
+        "memberName": "Miss Celia Walsh",
+        "jobTitle": "Senior Marketing Liaison"
+      },
+      {
+        "memberId": "4",
+        "memberName": "Jerald Blick",
+        "jobTitle": "International Directives Agent"
+      },
+      {
+        "memberId": "5",
+        "memberName": "Clint Lind-Simonis",
+        "jobTitle": "Senior Usability Liaison"
+      },
+      {
+        "memberId": "6",
+        "memberName": "Elizabeth Howe",
+        "jobTitle": "Chief Division Associate"
+      },
+      {
+        "memberId": "7",
+        "memberName": "Angel Mohr",
+        "jobTitle": "Product Applications Representative"
+      },
+      {
+        "memberId": "8",
+        "memberName": "Jeremy Fahey",
+        "jobTitle": "Principal Creative Planner"
+      },
+      {
+        "memberId": "9",
+        "memberName": "Edmund VonRueden",
+        "jobTitle": "Product Research Strategist"
+      },
+      {
+        "memberId": "10",
+        "memberName": "Ethel Nienow",
+        "jobTitle": "Senior Brand Technician"
+      }
+    ]
+  },
+  {
+    "id": "2",
+    "name": "Padberg - Boyer",
+    "private": true,
+    "location": "Reunion",
+    "members": [
+      {
+        "memberId": "1",
+        "memberName": "Jonathan Mraz",
+        "jobTitle": "Senior Solutions Liaison"
+      },
+      {
+        "memberId": "2",
+        "memberName": "Leslie Kozey-Heidenreich",
+        "jobTitle": "Customer Program Director"
+      },
+      {
+        "memberId": "3",
+        "memberName": "Luke Volkman",
+        "jobTitle": "Lead Operations Specialist"
+      },
+      {
+        "memberId": "4",
+        "memberName": "Ellis Dickens",
+        "jobTitle": "District Division Planner"
+      },
+      {
+        "memberId": "5",
+        "memberName": "Felipe Mann",
+        "jobTitle": "Lead Implementation Facilitator"
+      },
+      {
+        "memberId": "6",
+        "memberName": "Tonya Hauck",
+        "jobTitle": "Corporate Research Manager"
+      },
+      {
+        "memberId": "7",
+        "memberName": "Alberta Kovacek",
+        "jobTitle": "Principal Metrics Facilitator"
+      },
+      {
+        "memberId": "8",
+        "memberName": "Lorene Zemlak",
+        "jobTitle": "Customer Accounts Facilitator"
+      },
+      {
+        "memberId": "9",
+        "memberName": "Sadie Weissnat DVM",
+        "jobTitle": "Future Infrastructure Director"
+      },
+      {
+        "memberId": "10",
+        "memberName": "Christian Steuber",
+        "jobTitle": "Chief Factors Engineer"
+      }
+    ]
+  },
+  {
+    "id": "3",
+    "name": "Cole - Johnston",
+    "private": true,
+    "location": "Palau",
+    "members": [
+      {
+        "memberId": "1",
+        "memberName": "Cassandra Hoeger",
+        "jobTitle": "Investor Group Assistant"
+      },
+      {
+        "memberId": "2",
+        "memberName": "Ms. Dianna Hamill",
+        "jobTitle": "International Accountability Consultant"
+      },
+      {
+        "memberId": "3",
+        "memberName": "Dr. Clint Oberbrunner",
+        "jobTitle": "Corporate Implementation Specialist"
+      },
+      {
+        "memberId": "4",
+        "memberName": "Leslie Towne V",
+        "jobTitle": "National Security Architect"
+      },
+      {
+        "memberId": "5",
+        "memberName": "Michael Waelchi-Friesen DDS",
+        "jobTitle": "Customer Operations Consultant"
+      },
+      {
+        "memberId": "6",
+        "memberName": "Monique Bahringer",
+        "jobTitle": "Legacy Operations Coordinator"
+      },
+      {
+        "memberId": "7",
+        "memberName": "Jacqueline Bauch",
+        "jobTitle": "District Security Agent"
+      },
+      {
+        "memberId": "8",
+        "memberName": "Ryan Thiel",
+        "jobTitle": "Human Accountability Technician"
+      },
+      {
+        "memberId": "9",
+        "memberName": "Brendan Rice",
+        "jobTitle": "Legacy Operations Supervisor"
+      },
+      {
+        "memberId": "10",
+        "memberName": "Ramiro Homenick",
+        "jobTitle": "Dynamic Division Consultant"
+      }
+    ]
+  },
+  {
+    "id": "4",
+    "name": "Kuphal - Hickle",
+    "private": true,
+    "location": "Portugal",
+    "members": [
+      {
+        "memberId": "1",
+        "memberName": "Sergio Jenkins",
+        "jobTitle": "Regional Solutions Consultant"
+      },
+      {
+        "memberId": "2",
+        "memberName": "Abraham Heidenreich",
+        "jobTitle": "Lead Factors Engineer"
+      },
+      {
+        "memberId": "3",
+        "memberName": "Rodney Schulist",
+        "jobTitle": "Human Solutions Facilitator"
+      },
+      {
+        "memberId": "4",
+        "memberName": "Mr. Hugh Legros",
+        "jobTitle": "National Identity Technician"
+      },
+      {
+        "memberId": "5",
+        "memberName": "Angel Hudson",
+        "jobTitle": "Internal Program Engineer"
+      },
+      {
+        "memberId": "6",
+        "memberName": "Freddie Cormier",
+        "jobTitle": "Lead Response Officer"
+      },
+      {
+        "memberId": "7",
+        "memberName": "Bonnie Kulas",
+        "jobTitle": "International Assurance Orchestrator"
+      },
+      {
+        "memberId": "8",
+        "memberName": "Dolores Williamson",
+        "jobTitle": "Principal Directives Executive"
+      },
+      {
+        "memberId": "9",
+        "memberName": "Tommy Langosh",
+        "jobTitle": "Legacy Security Producer"
+      },
+      {
+        "memberId": "10",
+        "memberName": "John Zemlak",
+        "jobTitle": "Human Usability Facilitator"
+      }
+    ]
+  },
+  {
+    "id": "5",
+    "name": "Auer, Stroman and Kihn",
+    "private": false,
+    "location": "Italy",
+    "members": [
+      {
+        "memberId": "1",
+        "memberName": "Rita Ullrich",
+        "jobTitle": "Regional Infrastructure Facilitator"
+      },
+      {
+        "memberId": "2",
+        "memberName": "Lionel Lehner",
+        "jobTitle": "Forward Brand Manager"
+      },
+      {
+        "memberId": "3",
+        "memberName": "Grant Windler",
+        "jobTitle": "Customer Paradigm Liaison"
+      },
+      {
+        "memberId": "4",
+        "memberName": "Lorene Herzog",
+        "jobTitle": "Corporate Web Liaison"
+      },
+      {
+        "memberId": "5",
+        "memberName": "Trevor Harber",
+        "jobTitle": "Dynamic Group Strategist"
+      },
+      {
+        "memberId": "6",
+        "memberName": "Jon McDermott",
+        "jobTitle": "International Security Representative"
+      },
+      {
+        "memberId": "7",
+        "memberName": "Lionel Adams",
+        "jobTitle": "Regional Marketing Producer"
+      },
+      {
+        "memberId": "8",
+        "memberName": "Estelle Bechtelar",
+        "jobTitle": "Future Usability Agent"
+      },
+      {
+        "memberId": "9",
+        "memberName": "Henry Hegmann",
+        "jobTitle": "Senior Mobility Analyst"
+      },
+      {
+        "memberId": "10",
+        "memberName": "Claudia Raynor III",
+        "jobTitle": "Senior Implementation Supervisor"
+      }
+    ]
+  },
+  {
+    "id": "6",
+    "name": "Romaguera, Torp and Murray",
+    "private": false,
+    "location": "Bosnia and Herzegovina",
+    "members": [
+      {
+        "memberId": "1",
+        "memberName": "Sheryl Conn",
+        "jobTitle": "District Implementation Architect"
+      },
+      {
+        "memberId": "2",
+        "memberName": "Stella Nicolas",
+        "jobTitle": "Corporate Response Representative"
+      },
+      {
+        "memberId": "3",
+        "memberName": "Dr. Nelson Ferry",
+        "jobTitle": "Global Operations Planner"
+      },
+      {
+        "memberId": "4",
+        "memberName": "Nellie Larson",
+        "jobTitle": "Lead Branding Executive"
+      },
+      {
+        "memberId": "5",
+        "memberName": "Julia Jenkins",
+        "jobTitle": "Dynamic Operations Liaison"
+      },
+      {
+        "memberId": "6",
+        "memberName": "Sabrina Grady",
+        "jobTitle": "National Program Manager"
+      },
+      {
+        "memberId": "7",
+        "memberName": "Joanne Zemlak",
+        "jobTitle": "National Infrastructure Producer"
+      },
+      {
+        "memberId": "8",
+        "memberName": "Arturo Hodkiewicz I",
+        "jobTitle": "Legacy Creative Strategist"
+      },
+      {
+        "memberId": "9",
+        "memberName": "Roman Barrows",
+        "jobTitle": "District Metrics Officer"
+      },
+      {
+        "memberId": "10",
+        "memberName": "Martha Koch V",
+        "jobTitle": "Internal Accountability Architect"
+      }
+    ]
+  },
+  {
+    "id": "7",
+    "name": "Lindgren, Hansen and Considine",
+    "private": true,
+    "location": "Uzbekistan",
+    "members": [
+      {
+        "memberId": "1",
+        "memberName": "Ann Harber",
+        "jobTitle": "Legacy Marketing Agent"
+      },
+      {
+        "memberId": "2",
+        "memberName": "Pablo Hirthe",
+        "jobTitle": "Forward Implementation Facilitator"
+      },
+      {
+        "memberId": "3",
+        "memberName": "Ramon Schumm",
+        "jobTitle": "Dynamic Web Representative"
+      },
+      {
+        "memberId": "4",
+        "memberName": "Rachel Mraz",
+        "jobTitle": "District Markets Facilitator"
+      },
+      {
+        "memberId": "5",
+        "memberName": "Mae Schmitt",
+        "jobTitle": "Senior Markets Director"
+      },
+      {
+        "memberId": "6",
+        "memberName": "Veronica Pagac",
+        "jobTitle": "Chief Infrastructure Assistant"
+      },
+      {
+        "memberId": "7",
+        "memberName": "Marlon Johnson",
+        "jobTitle": "Dynamic Accounts Administrator"
+      },
+      {
+        "memberId": "8",
+        "memberName": "Johnathan Daugherty Sr.",
+        "jobTitle": "International Optimization Strategist"
+      },
+      {
+        "memberId": "9",
+        "memberName": "Oliver Shanahan",
+        "jobTitle": "Legacy Mobility Agent"
+      },
+      {
+        "memberId": "10",
+        "memberName": "Curtis Jakubowski",
+        "jobTitle": "Lead Research Orchestrator"
+      }
+    ]
+  },
+  {
+    "id": "8",
+    "name": "Buckridge, Jakubowski and Borer",
+    "private": false,
+    "location": "Colombia",
+    "members": [
+      {
+        "memberId": "1",
+        "memberName": "Angel Medhurst",
+        "jobTitle": "Forward Marketing Producer"
+      },
+      {
+        "memberId": "2",
+        "memberName": "Jody Harvey",
+        "jobTitle": "District Research Analyst"
+      },
+      {
+        "memberId": "3",
+        "memberName": "Lucy Dach V",
+        "jobTitle": "Regional Accounts Engineer"
+      },
+      {
+        "memberId": "4",
+        "memberName": "Henry Schmidt-Cruickshank",
+        "jobTitle": "Dynamic Infrastructure Administrator"
+      },
+      {
+        "memberId": "5",
+        "memberName": "Steven Quitzon MD",
+        "jobTitle": "Chief Configuration Consultant"
+      },
+      {
+        "memberId": "6",
+        "memberName": "Mrs. Miriam McGlynn",
+        "jobTitle": "Chief Solutions Engineer"
+      },
+      {
+        "memberId": "7",
+        "memberName": "Dr. Brad Kub",
+        "jobTitle": "Investor Program Agent"
+      },
+      {
+        "memberId": "8",
+        "memberName": "Barry Cronin",
+        "jobTitle": "Internal Marketing Architect"
+      },
+      {
+        "memberId": "9",
+        "memberName": "Maryann Tremblay",
+        "jobTitle": "Dynamic Configuration Manager"
+      },
+      {
+        "memberId": "10",
+        "memberName": "Roman Ziemann",
+        "jobTitle": "Human Data Architect"
+      }
+    ]
+  },
+  {
+    "id": "9",
+    "name": "Howe - Thiel",
+    "private": true,
+    "location": "Palau",
+    "members": [
+      {
+        "memberId": "1",
+        "memberName": "Armando Kohler",
+        "jobTitle": "Corporate Tactics Director"
+      },
+      {
+        "memberId": "2",
+        "memberName": "Jamie Macejkovic",
+        "jobTitle": "Investor Optimization Facilitator"
+      },
+      {
+        "memberId": "3",
+        "memberName": "Melissa Bernier",
+        "jobTitle": "Dynamic Functionality Supervisor"
+      },
+      {
+        "memberId": "4",
+        "memberName": "Erma Collins",
+        "jobTitle": "Direct Factors Representative"
+      },
+      {
+        "memberId": "5",
+        "memberName": "Bill Williamson",
+        "jobTitle": "Investor Intranet Developer"
+      },
+      {
+        "memberId": "6",
+        "memberName": "Reginald Hermiston",
+        "jobTitle": "Lead Security Developer"
+      },
+      {
+        "memberId": "7",
+        "memberName": "Cornelius Schuster",
+        "jobTitle": "Senior Group Designer"
+      },
+      {
+        "memberId": "8",
+        "memberName": "Ramon Monahan IV",
+        "jobTitle": "Direct Web Agent"
+      },
+      {
+        "memberId": "9",
+        "memberName": "Rafael Koelpin DVM",
+        "jobTitle": "Dynamic Assurance Executive"
+      },
+      {
+        "memberId": "10",
+        "memberName": "Delores Quitzon",
+        "jobTitle": "Global Metrics Architect"
+      }
+    ]
+  }
+]
+
+export default groups;
