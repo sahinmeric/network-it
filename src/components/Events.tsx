b2d49474195633c4bb798f536ccb9bba68d537d7
sahinmeric <sahin.meric86@gmail.com>
2023-11-09 22:44:41 +0100

    events and groups components created

---
+++ src/components/Events.tsx
@@ -0,0 +1,8 @@
+// Events.tsx
+import React from "react";
+
+const Events: React.FC = () => {
+  return <div>Events Section</div>;
+};
+
+export default Events;
---
+++ src/components/Groups.tsx
@@ -0,0 +1,8 @@
+// Groups.tsx
+import React from "react";
+
+const Groups: React.FC = () => {
+  return <div>Groups Section</div>;
+};
+
+export default Groups;
