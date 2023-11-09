ab8d744142cd882e5402b76b09e7488f6b2785d3
sahinmeric <sahin.meric86@gmail.com>
2023-11-10 00:38:27 +0100

    showing groups and basic search added

--- src/components/Groups.tsx
+++ src/components/Groups.tsx
@@ -1,8 +1,49 @@
-// Groups.tsx
-import React from "react";
+// components/Groups.js
+import React, { useState } from "react";
+import groupsData from "../mockdata/groups";
 
-const Groups: React.FC = () => {
-  return <div>Groups Section</div>;
+const Groups = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState(groupsData);
+
+  const handleSearch = (e: any) => {
+    const term = e.target.value;
+    setSearchTerm(term);
+
+    // Filter groups based on the search term
+    const filteredGroups = groupsData.filter((group) =>
+      group.name.toLowerCase().includes(term.toLowerCase())
+    );
+
+    setSearchResults(filteredGroups);
+  };
+
+  return (
+    <div>
+      <h2>Groups</h2>
+      <input
+        type="text"
+        placeholder="Search groups..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+
+      {searchResults.map((group) => (
+        <div key={group.id}>
+          <h3>{group.name}</h3>
+          <p>Location: {group.location}</p>
+          {/* Display other group information */}
+          <ul>
+            {group.members.map((member) => (
+              <li key={member.memberId}>
+                {member.memberName} - {member.jobTitle}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Groups;
