bd58c3e9cb08d12446c01fea78f1a366b45cb15c
sahinmeric <sahin.meric86@gmail.com>
2023-11-11 16:43:29 +0100

    added mock feed data

---
+++ src/mockdata/feeds.js
@@ -0,0 +1,50 @@
+const feeds = [
+  {
+    memberId: 1,
+    memberName: "John Doe",
+    timestamp: "2 hours ago",
+    type: "note",
+    content: "Well, this is my first post on this network!",
+  },
+  {
+    memberId: 2,
+    memberName: "Jane Doe",
+    timestamp: "3 hours ago",
+    type: "video",
+    content: "Check out this cool video I found on the internet!",
+    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  },
+  {
+    memberId: 3,
+    memberName: "Jordan Doe",
+    timestamp: "5 hours ago",
+    type: "image",
+    content: "Here's a picture of my cat",
+    link: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg",
+  },
+  {
+    memberId: 4,
+    memberName: "Jill Doe",
+    timestamp: "6 hours ago",
+    type: "note",
+    content: "I'm so excited to be a part of this network!",
+  },
+  {
+    memberId: 5,
+    memberName: "Jack Doe",
+    timestamp: "7 hours ago",
+    type: "video",
+    content: "I made a video about my favorite game!",
+    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  },
+  {
+    memberId: 6,
+    memberName: "Jen Doe",
+    timestamp: "8 hours ago",
+    type: "image",
+    content: "Here's a picture of my dog and her puppies",
+    link: "https://upload.wikimedia.org/wikipedia/commons/f/f1/S%C3%A4ugende_H%C3%BCndin.JPG",
+  },
+];
+
+export default feeds;
