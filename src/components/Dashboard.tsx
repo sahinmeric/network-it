03303dba6a880ecc7b583365e3bf6099a14180b6
sahinmeric <sahin.meric86@gmail.com>
2023-11-11 16:44:43 +0100

    Added feed to dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,16 @@
 // Dashboard.tsx
 import React from "react";
-import { AppBar, Tabs, Tab, Container, Box } from "@mui/material";
+import {
+  AppBar,
+  Tabs,
+  Tab,
+  Container,
+  Box,
+  Paper,
+  Typography,
+} from "@mui/material";
 import { Link, Outlet } from "react-router-dom";
+import feeds from "../mockdata/feeds";
 
 const Dashboard: React.FC = () => {
   return (
@@ -15,6 +24,39 @@ const Dashboard: React.FC = () => {
       </AppBar>
       <Container>
         <Outlet /> {/* Render nested routes */}
+        <Paper elevation={3} style={{ padding: "20px", marginTop: "20px" }}>
+          <Typography variant="h6" gutterBottom>
+            Social Updates
+          </Typography>
+          {feeds.map((item) => (
+            <div key={item.memberId}>
+              <Typography variant="subtitle1">{item.memberName}</Typography>
+              <Typography variant="body2" color="textSecondary">
+                {item.timestamp}
+              </Typography>
+              {item.type === "note" && <Typography>{item.content}</Typography>}
+              {item.type === "video" && (
+                <div>
+                  <Typography>{item.content}</Typography>
+                  <a href={item.link} target="_blank" rel="noopener noreferrer">
+                    Watch the video
+                  </a>
+                </div>
+              )}
+              {item.type === "image" && (
+                <div>
+                  <Typography>{item.content}</Typography>
+                  <img
+                    src={item.link}
+                    alt="Meme"
+                    style={{ maxWidth: "200px" }}
+                  />
+                </div>
+              )}
+              <hr style={{ margin: "10px 0" }} />
+            </div>
+          ))}
+        </Paper>
       </Container>
     </Box>
   );
