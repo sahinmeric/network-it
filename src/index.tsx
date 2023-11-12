e6d05df58479c7ab9973dc7f1133773ee68528f9
sahinmeric <sahin.meric86@gmail.com>
2023-11-12 23:11:45 +0100

    index css added, theme provider added

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createRoot } from "react-dom/client";
+import AppRouter from "./AppRouter";
+import { GlobalContextProvider } from "./components/Context/GlobalContext";
+import theme from "./config/Theme";
+import "./index.css";
+
+const container = document.getElementById("root");
+
+const root = createRoot(container!);
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalContextProvider>
+        <Router>
+          <CssBaseline />
+          <AppRouter />
+        </Router>
+      </GlobalContextProvider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
