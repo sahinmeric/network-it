a98694f96d9c47670971002eecc86f10f9f58276
sahinmeric <sahin.meric86@gmail.com>
2023-11-12 12:07:14 +0100

    home refactored, abstracted header

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,46 +1,25 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Button, Container } from "@mui/material";
 import LoginDialog from "./components/LoginDialog";
 import SignupDialog from "./components/SignupDialog";
 
-const Home: React.FC = () => {
+const Home = () => {
   const navigate = useNavigate();
   const [loginOpen, setLoginOpen] = useState(false);
   const [signupOpen, setSignupOpen] = useState(false);
 
   const handleLoginSuccess = () => {
-    // Close the login dialog
     setLoginOpen(false);
-    // Redirect to the profile page
-    navigate("/profile");
-    //history.push("/profile");
+    navigate("/feed");
   };
 
   const handleSignupSuccess = () => {
-    // Close the signup dialog
     setSignupOpen(false);
-    // Redirect to the profile page
     navigate("/profile");
-    //history.push("/profile");
   };
 
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" style={{ flexGrow: 1 }}>
-            Network-IT
-          </Typography>
-          <Button color="inherit" onClick={() => setSignupOpen(true)}>
-            Signup
-          </Button>
-          <Button color="inherit" onClick={() => setLoginOpen(true)}>
-            Login
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <Container>{/* Page content goes here */}</Container>
+    <>
       <LoginDialog
         open={loginOpen}
         onClose={() => setLoginOpen(false)}
@@ -51,7 +30,7 @@ const Home: React.FC = () => {
         onClose={() => setSignupOpen(false)}
         onSignupSuccess={handleSignupSuccess}
       />
-    </div>
+    </>
   );
 };
 
