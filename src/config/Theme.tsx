999f372974b4f480695e606e8f422333618e6b40
sahinmeric <sahin.meric86@gmail.com>
2023-11-12 23:16:47 +0100

    theme added

---
+++ src/config/Theme.tsx
@@ -0,0 +1,72 @@
+import { createTheme } from "@mui/material";
+
+import colors from "./colors";
+
+const themeOptions = createTheme({
+  typography: {
+    h1: {
+      fontFamily: "LarishNeueSemibold",
+      fontSize: 20,
+      lineHeight: 1.2,
+      letterSpacing: "0.15px",
+      marginBottom: "1.2em",
+    },
+    h2: {
+      fontSize: 18,
+      lineHeight: 1.33,
+      letterSpacing: "normal",
+      fontWeight: "normal",
+    },
+    h3: {
+      fontSize: 16,
+      lineHeight: 1.5,
+      letterSpacing: "0.12px",
+      fontWeight: 500,
+    },
+    h4: {
+      fontSize: 18,
+      lineHeight: 1.33,
+      letterSpacing: "normal",
+      fontWeight: "normal",
+      textTransform: "uppercase",
+    },
+    body1: {
+      fontSize: 14,
+      lineHeight: 1.3,
+      letterSpacing: "normal",
+    },
+    body2: {
+      fontSize: 14,
+      lineHeight: 1.3,
+      letterSpacing: "normal",
+    },
+    subtitle1: {
+      fontSize: 12,
+      fontWeight: "300",
+    },
+    subtitle2: {
+      fontSize: 14,
+      fontWeight: "300",
+      marginTop: "10px",
+    },
+    fontFamily: "Euclid Circular B",
+    fontSize: 14,
+  },
+  palette: {
+    primary: {
+      main: colors.primary,
+    },
+    secondary: {
+      main: colors.white,
+    },
+    text: {
+      primary: colors.dark,
+      secondary: colors.dark,
+    },
+    error: {
+      main: colors.error,
+    },
+  },
+});
+
+export default themeOptions;
