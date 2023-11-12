a10f104b1eba1884c05f058744928e46c9c9e331
sahinmeric <sahin.meric86@gmail.com>
2023-11-12 12:07:36 +0100

    global context added

---
+++ src/components/Context/GlobalContext.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+interface GlobalContextValue {
+  isLoggedIn: boolean;
+  setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const GlobalContext = React.createContext<GlobalContextValue | undefined>(
+  undefined
+);
+
+export const useGlobalContext = () => {
+  const globalContext = React.useContext(GlobalContext);
+
+  if (globalContext === undefined) {
+    throw new Error("useGlobalContext must be inside a GlobalProvider");
+  }
+
+  return globalContext;
+};
+
+interface GlobalContextProviderProps {
+  children: React.ReactNode;
+}
+
+export const GlobalContextProvider: React.FC<GlobalContextProviderProps> = ({
+  children,
+}) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <GlobalContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export default GlobalContext;
