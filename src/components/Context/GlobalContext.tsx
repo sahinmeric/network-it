87a259984a43199b9a26b190e1aaf27c018ce715
sahinmeric <sahin.meric86@gmail.com>
2023-11-20 12:19:28 +0100

    to open login and signup dialog states are moved to global context

--- src/components/Context/GlobalContext.tsx
+++ src/components/Context/GlobalContext.tsx
@@ -3,7 +3,11 @@ import React, { useState } from "react";
 
 interface GlobalContextValue {
   isLoggedIn: boolean;
+  loginOpen: boolean;
+  signupOpen: boolean;
   setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+  setLoginOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setSignupOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const GlobalContext = React.createContext<GlobalContextValue | undefined>(
@@ -28,9 +32,20 @@ export const GlobalContextProvider: React.FC<GlobalContextProviderProps> = ({
   children,
 }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loginOpen, setLoginOpen] = useState(false);
+  const [signupOpen, setSignupOpen] = useState(false);
 
   return (
-    <GlobalContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+    <GlobalContext.Provider
+      value={{
+        isLoggedIn,
+        loginOpen,
+        signupOpen,
+        setIsLoggedIn,
+        setLoginOpen,
+        setSignupOpen,
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   );
