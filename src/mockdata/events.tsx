2be66ab7d91b7ea2377d3a04645495a44d4082b5
sahinmeric <sahin.meric86@gmail.com>
2023-11-26 21:42:20 +0100

    file type changed to tsx

---
+++ src/mockdata/events.tsx
@@ -0,0 +1,626 @@
+import { IEvent } from "../components/interfaces/Interfaces";
+
+const events: IEvent[] = [
+  {
+    id: "1",
+    name: "Public-key asynchronous focus group Event",
+    date: "2024-07-15T14:29:51.953Z",
+    location: "2378 Yasmine Corner",
+    description:
+      "Doloribus verbum comburo casus repellendus tenus. Ullam quia substantia accusator pecto spectaculum crux arx tergo dedico. Suppono vomica talus suggero cohors minima pauper cruentus comedo.",
+    organizer: "Patsy Moore",
+    private: false,
+    members: [
+      {
+        memberId: "1",
+        memberName: "Vivian Wisoky",
+        jobTitle: "Chief Optimization Manager",
+      },
+      {
+        memberId: "2",
+        memberName: "Erma Block",
+        jobTitle: "Legacy Factors Orchestrator",
+      },
+      {
+        memberId: "3",
+        memberName: "Terry Mosciski III",
+        jobTitle: "Internal Marketing Representative",
+      },
+      {
+        memberId: "4",
+        memberName: "Darrell Becker",
+        jobTitle: "Customer Marketing Orchestrator",
+      },
+      {
+        memberId: "5",
+        memberName: "Leigh Doyle",
+        jobTitle: "Product Web Orchestrator",
+      },
+      {
+        memberId: "6",
+        memberName: "Erin Schinner",
+        jobTitle: "Regional Paradigm Agent",
+      },
+      {
+        memberId: "7",
+        memberName: "Clinton Torphy",
+        jobTitle: "District Configuration Designer",
+      },
+      {
+        memberId: "8",
+        memberName: "Donna Mitchell",
+        jobTitle: "Customer Usability Administrator",
+      },
+      {
+        memberId: "9",
+        memberName: "Veronica Reichert",
+        jobTitle: "Internal Applications Associate",
+      },
+      {
+        memberId: "10",
+        memberName: "Darin Watsica",
+        jobTitle: "Future Directives Supervisor",
+      },
+    ],
+  },
+  {
+    id: "2",
+    name: "Configurable content-based interface Event",
+    date: "2023-11-29T19:09:24.864Z",
+    location: "8310 Erdman Fords",
+    description:
+      "Sonitus animi voveo tamisium arcesso. Caput cernuus amita textor supplanto crastinus adulescens eos trucido. Similique esse assentator color agnosco demulceo.",
+    organizer: "Dustin Wolf",
+    private: true,
+    members: [
+      {
+        memberId: "1",
+        memberName: "Salvatore Gusikowski",
+        jobTitle: "Principal Branding Strategist",
+      },
+      {
+        memberId: "2",
+        memberName: "Oscar Kuhlman",
+        jobTitle: "Central Accounts Liaison",
+      },
+      {
+        memberId: "3",
+        memberName: "Johnnie Howe",
+        jobTitle: "Regional Usability Administrator",
+      },
+      {
+        memberId: "4",
+        memberName: "Dewey Cronin",
+        jobTitle: "Principal Brand Strategist",
+      },
+      {
+        memberId: "5",
+        memberName: "Kristen Nicolas",
+        jobTitle: "Product Usability Consultant",
+      },
+      {
+        memberId: "6",
+        memberName: "Ed Bernier",
+        jobTitle: "Central Research Assistant",
+      },
+      {
+        memberId: "7",
+        memberName: "Pablo Keebler",
+        jobTitle: "National Identity Coordinator",
+      },
+      {
+        memberId: "8",
+        memberName: "Adrienne Abernathy MD",
+        jobTitle: "Chief Security Coordinator",
+      },
+      {
+        memberId: "9",
+        memberName: "Lucas Bernhard",
+        jobTitle: "International Branding Associate",
+      },
+      {
+        memberId: "10",
+        memberName: "Alicia Paucek",
+        jobTitle: "Central Security Director",
+      },
+    ],
+  },
+  {
+    id: "3",
+    name: "Switchable human-resource database Event",
+    date: "2023-12-07T01:14:17.099Z",
+    location: "66402 13th Street",
+    description:
+      "Sopor solus curto debilito. Pax atavus combibo commodo stillicidium amicitia. Ipsam tergeo arbitro calco derelinquo.",
+    organizer: "Melvin Schultz",
+    private: true,
+    members: [
+      {
+        memberId: "1",
+        memberName: "Miriam Huels",
+        jobTitle: "Central Markets Facilitator",
+      },
+      {
+        memberId: "2",
+        memberName: "Mr. Sammy Kovacek",
+        jobTitle: "Customer Accountability Orchestrator",
+      },
+      {
+        memberId: "3",
+        memberName: "Eduardo Stiedemann",
+        jobTitle: "Dynamic Creative Associate",
+      },
+      {
+        memberId: "4",
+        memberName: "Hugo Schultz",
+        jobTitle: "Senior Mobility Liaison",
+      },
+      {
+        memberId: "5",
+        memberName: "Melvin Osinski",
+        jobTitle: "Direct Tactics Supervisor",
+      },
+      {
+        memberId: "6",
+        memberName: "Susie Jakubowski",
+        jobTitle: "National Tactics Consultant",
+      },
+      {
+        memberId: "7",
+        memberName: "Katie Fritsch",
+        jobTitle: "Investor Communications Coordinator",
+      },
+      {
+        memberId: "8",
+        memberName: "Ira Grimes",
+        jobTitle: "Global Operations Supervisor",
+      },
+      {
+        memberId: "9",
+        memberName: "Wilbert Schamberger PhD",
+        jobTitle: "National Data Specialist",
+      },
+      {
+        memberId: "10",
+        memberName: "Anita King",
+        jobTitle: "National Research Designer",
+      },
+    ],
+  },
+  {
+    id: "4",
+    name: "Managed system-worthy toolset Event",
+    date: "2024-04-29T01:59:37.553Z",
+    location: "499 Maple Avenue",
+    description:
+      "Ultio degenero desolo. Vacuus volutabrum at corroboro cado quos copiose cotidie vergo. Deludo cohibeo surculus adduco infit.",
+    organizer: "Leslie Smitham",
+    private: true,
+    members: [
+      {
+        memberId: "1",
+        memberName: "Dianna Johns",
+        jobTitle: "Central Implementation Representative",
+      },
+      {
+        memberId: "2",
+        memberName: "Yvette Abshire MD",
+        jobTitle: "Internal Operations Facilitator",
+      },
+      {
+        memberId: "3",
+        memberName: "Kristen Kutch",
+        jobTitle: "Central Group Analyst",
+      },
+      {
+        memberId: "4",
+        memberName: "Tracy Grimes",
+        jobTitle: "Future Division Representative",
+      },
+      {
+        memberId: "5",
+        memberName: "Colleen Dooley IV",
+        jobTitle: "Product Directives Supervisor",
+      },
+      {
+        memberId: "6",
+        memberName: "Ann Gorczany",
+        jobTitle: "Human Assurance Consultant",
+      },
+      {
+        memberId: "7",
+        memberName: "Aaron Adams",
+        jobTitle: "Forward Paradigm Planner",
+      },
+      {
+        memberId: "8",
+        memberName: "Dr. Paul Runte",
+        jobTitle: "Regional Usability Engineer",
+      },
+      {
+        memberId: "9",
+        memberName: "Milton Hodkiewicz",
+        jobTitle: "Legacy Applications Planner",
+      },
+      {
+        memberId: "10",
+        memberName: "Don Swift",
+        jobTitle: "Investor Configuration Producer",
+      },
+    ],
+  },
+  {
+    id: "5",
+    name: "Ameliorated background protocol Event",
+    date: "2024-10-23T02:43:51.711Z",
+    location: "55293 Jackson Avenue",
+    description:
+      "Eos abutor optio. Debeo tenuis aiunt aro. Aperio theologus deserunt capitulus umbra.",
+    organizer: "Debbie Jacobson",
+    private: true,
+    members: [
+      {
+        memberId: "1",
+        memberName: "Matthew Quitzon",
+        jobTitle: "Chief Security Planner",
+      },
+      {
+        memberId: "2",
+        memberName: "Wilma Schroeder",
+        jobTitle: "Legacy Markets Associate",
+      },
+      {
+        memberId: "3",
+        memberName: "Fernando Fritsch",
+        jobTitle: "Global Branding Specialist",
+      },
+      {
+        memberId: "4",
+        memberName: "Duane Hane",
+        jobTitle: "Investor Configuration Officer",
+      },
+      {
+        memberId: "5",
+        memberName: "Randall Dare",
+        jobTitle: "Customer Branding Director",
+      },
+      {
+        memberId: "6",
+        memberName: "Merle Nicolas",
+        jobTitle: "Principal Response Strategist",
+      },
+      {
+        memberId: "7",
+        memberName: "Renee Treutel III",
+        jobTitle: "International Paradigm Executive",
+      },
+      {
+        memberId: "8",
+        memberName: "Mark Blanda",
+        jobTitle: "Forward Identity Specialist",
+      },
+      {
+        memberId: "9",
+        memberName: "Allen Schimmel",
+        jobTitle: "Senior Marketing Developer",
+      },
+      {
+        memberId: "10",
+        memberName: "Miranda Ritchie",
+        jobTitle: "Lead Intranet Planner",
+      },
+    ],
+  },
+  {
+    id: "6",
+    name: "Enterprise-wide high-level installation Event",
+    date: "2024-07-14T16:44:47.955Z",
+    location: "2745 Hickle Cove",
+    description:
+      "Soleo utrimque volva adsum audentia tollo eos coniecto admitto capillus. Verumtamen ancilla temperantia suus spes certus caste tepesco sollicito. Aspernatur suus capto aperiam vinculum conspergo compello amicitia vir.",
+    organizer: "Jim Rice",
+    private: true,
+    members: [
+      {
+        memberId: "1",
+        memberName: "Ron Runolfsdottir",
+        jobTitle: "Dynamic Integration Facilitator",
+      },
+      {
+        memberId: "2",
+        memberName: "Iris Wisoky",
+        jobTitle: "Senior Factors Director",
+      },
+      {
+        memberId: "3",
+        memberName: "Dianna Balistreri-Quigley",
+        jobTitle: "Regional Accountability Executive",
+      },
+      {
+        memberId: "4",
+        memberName: "Jorge Bechtelar",
+        jobTitle: "Central Marketing Coordinator",
+      },
+      {
+        memberId: "5",
+        memberName: "Mr. Travis Miller",
+        jobTitle: "Future Mobility Producer",
+      },
+      {
+        memberId: "6",
+        memberName: "Dr. Walter Hand I",
+        jobTitle: "Lead Web Developer",
+      },
+      {
+        memberId: "7",
+        memberName: "Ted Kunze",
+        jobTitle: "National Interactions Analyst",
+      },
+      {
+        memberId: "8",
+        memberName: "Lydia Thompson",
+        jobTitle: "Global Operations Facilitator",
+      },
+      {
+        memberId: "9",
+        memberName: "Kurt Upton",
+        jobTitle: "Customer Response Consultant",
+      },
+      {
+        memberId: "10",
+        memberName: "Dr. Emanuel Ledner-Schmitt",
+        jobTitle: "Central Creative Engineer",
+      },
+    ],
+  },
+  {
+    id: "7",
+    name: "Reduced needs-based analyzer Event",
+    date: "2024-07-16T13:31:04.803Z",
+    location: "7247 Kingfisher Close",
+    description:
+      "Ante aspicio desidero vestrum blandior inventore conduco vado. Barba contabesco corrumpo acerbitas. Tam admiratio peccatus cursus aestus comedo delinquo caritas validus.",
+    organizer: "Miss Betsy Daugherty",
+    private: true,
+    members: [
+      {
+        memberId: "1",
+        memberName: "Wallace Murazik",
+        jobTitle: "Global Factors Officer",
+      },
+      {
+        memberId: "2",
+        memberName: "Oliver Erdman",
+        jobTitle: "Internal Mobility Planner",
+      },
+      {
+        memberId: "3",
+        memberName: "Henrietta Zulauf",
+        jobTitle: "Customer Creative Representative",
+      },
+      {
+        memberId: "4",
+        memberName: "Nancy Dicki",
+        jobTitle: "Chief Quality Manager",
+      },
+      {
+        memberId: "5",
+        memberName: "Daisy Haag",
+        jobTitle: "Investor Marketing Orchestrator",
+      },
+      {
+        memberId: "6",
+        memberName: "Miss Shelia Feil-Roberts",
+        jobTitle: "Customer Integration Specialist",
+      },
+      {
+        memberId: "7",
+        memberName: "Deanna Schaden MD",
+        jobTitle: "Direct Creative Officer",
+      },
+      {
+        memberId: "8",
+        memberName: "Ana Lehner",
+        jobTitle: "Legacy Infrastructure Director",
+      },
+      {
+        memberId: "9",
+        memberName: "Ms. Erin Kunde III",
+        jobTitle: "Future Usability Technician",
+      },
+      {
+        memberId: "10",
+        memberName: "Lynette Franecki",
+        jobTitle: "Direct Usability Designer",
+      },
+    ],
+  },
+  {
+    id: "8",
+    name: "Versatile eco-centric Graphical User Interface Event",
+    date: "2024-03-26T03:49:53.930Z",
+    location: "6606 Spring Gardens",
+    description:
+      "Desipio demens votum vehemens asperiores. Vestigium appono arguo utroque adstringo defendo vicinus. Cornu calcar tenetur texo atavus arma clarus.",
+    organizer: "Dr. Geoffrey Stokes",
+    private: true,
+    members: [
+      {
+        memberId: "1",
+        memberName: "Mrs. Josephine Jacobs III",
+        jobTitle: "Corporate Quality Administrator",
+      },
+      {
+        memberId: "2",
+        memberName: "Tommie Feest",
+        jobTitle: "Regional Directives Liaison",
+      },
+      {
+        memberId: "3",
+        memberName: "Robyn Davis",
+        jobTitle: "Future Integration Architect",
+      },
+      {
+        memberId: "4",
+        memberName: "Jan Durgan",
+        jobTitle: "District Creative Administrator",
+      },
+      {
+        memberId: "5",
+        memberName: "Irving McGlynn",
+        jobTitle: "Principal Paradigm Developer",
+      },
+      {
+        memberId: "6",
+        memberName: "Maureen Dickens",
+        jobTitle: "Senior Branding Manager",
+      },
+      {
+        memberId: "7",
+        memberName: "Mercedes McGlynn-Reichert",
+        jobTitle: "Product Security Planner",
+      },
+      {
+        memberId: "8",
+        memberName: "Mrs. Kimberly Blanda",
+        jobTitle: "National Infrastructure Specialist",
+      },
+      {
+        memberId: "9",
+        memberName: "Mr. Jon Krajcik IV",
+        jobTitle: "National Paradigm Facilitator",
+      },
+      {
+        memberId: "10",
+        memberName: "Colleen Schamberger",
+        jobTitle: "Dynamic Markets Specialist",
+      },
+    ],
+  },
+  {
+    id: "9",
+    name: "Persistent high-level project Event",
+    date: "2024-06-10T14:39:10.530Z",
+    location: "41762 Carroll Ways",
+    description:
+      "Crastinus viriliter iure accedo. Eos tenax verus caecus utor rerum creo taceo. Voluptatem pax cohors assentator capto ulterius natus.",
+    organizer: "Brian Ratke Jr.",
+    private: true,
+    members: [
+      {
+        memberId: "1",
+        memberName: "Dr. Blanca Witting III",
+        jobTitle: "Investor Web Representative",
+      },
+      {
+        memberId: "2",
+        memberName: "Carl Toy-Barrows",
+        jobTitle: "Legacy Directives Representative",
+      },
+      {
+        memberId: "3",
+        memberName: "Roxanne Simonis",
+        jobTitle: "Lead Integration Representative",
+      },
+      {
+        memberId: "4",
+        memberName: "Miss Laura Nikolaus",
+        jobTitle: "Principal Creative Specialist",
+      },
+      {
+        memberId: "5",
+        memberName: "Mrs. Marta Marvin",
+        jobTitle: "Internal Directives Planner",
+      },
+      {
+        memberId: "6",
+        memberName: "Dr. Hattie Ritchie",
+        jobTitle: "District Group Director",
+      },
+      {
+        memberId: "7",
+        memberName: "Dr. Charlie Leuschke",
+        jobTitle: "Forward Response Planner",
+      },
+      {
+        memberId: "8",
+        memberName: "Mario Mitchell-Ziemann",
+        jobTitle: "Chief Branding Supervisor",
+      },
+      {
+        memberId: "9",
+        memberName: "Frank Schumm",
+        jobTitle: "Legacy Creative Orchestrator",
+      },
+      {
+        memberId: "10",
+        memberName: "Miss Myra Ortiz",
+        jobTitle: "Regional Accounts Orchestrator",
+      },
+    ],
+  },
+  {
+    id: "10",
+    name: "Versatile actuating workforce Event",
+    date: "2024-04-10T04:11:08.212Z",
+    location: "8700 Dariana Cape",
+    description:
+      "Corporis depromo uterque eaque paulatim. Vita cunabula cursus vitae urbanus. Vix adipisci aliqua sursum aeternus trans decens averto demonstro dolores.",
+    organizer: "Stacey Lockman",
+    private: true,
+    members: [
+      {
+        memberId: "1",
+        memberName: "Dr. Guadalupe Strosin",
+        jobTitle: "Investor Paradigm Representative",
+      },
+      {
+        memberId: "2",
+        memberName: "Lee Barrows",
+        jobTitle: "Direct Security Supervisor",
+      },
+      {
+        memberId: "3",
+        memberName: "Hazel Heathcote",
+        jobTitle: "Central Quality Representative",
+      },
+      {
+        memberId: "4",
+        memberName: "Pablo Kertzmann",
+        jobTitle: "International Program Executive",
+      },
+      {
+        memberId: "5",
+        memberName: "Patricia Windler MD",
+        jobTitle: "Chief Functionality Agent",
+      },
+      {
+        memberId: "6",
+        memberName: "Vernon Kuhic",
+        jobTitle: "Chief Security Designer",
+      },
+      {
+        memberId: "7",
+        memberName: "Becky Dicki",
+        jobTitle: "Regional Program Executive",
+      },
+      {
+        memberId: "8",
+        memberName: "Jasmine Rau",
+        jobTitle: "Customer Security Agent",
+      },
+      {
+        memberId: "9",
+        memberName: "Alberta Casper",
+        jobTitle: "Future Infrastructure Specialist",
+      },
+      {
+        memberId: "10",
+        memberName: "Christie Von",
+        jobTitle: "Product Implementation Coordinator",
+      },
+    ],
+  },
+];
+
+export default events;
